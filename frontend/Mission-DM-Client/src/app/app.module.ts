00b79cd9d3957cf7aa4eca2dc7ff775570beeb3f
Ed Tischler <etischler@ufl.edu>
2018-03-24 16:14:42 -0400

    Base Footer Done

--- frontend/Mission-DM-Client/src/app/app.module.ts
+++ frontend/Mission-DM-Client/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { AlertModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
+import { FootbarComponent } from './footbar/footbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FootbarComponent
   ],
   imports: [
     BrowserModule,
